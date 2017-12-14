c47ed0ffb4550da7e97bc6064edd3ac012876d06
Woratham Khangtragool <worathamkh@gmail.com>
2017-12-14 10:54:12 +0700

    change random in /api/convention/gen

--- routes/convention.js
+++ routes/convention.js
@@ -6,6 +6,8 @@ var randomParagraph = require('random-paragraph');
 var moment = require('moment');
 var randomMoment = require('moment-random');
 var orm = require('orm');
+var R = require('random-js');
+var E = R.engines.mt19937().autoSeed();
 
 router.get('/all', (req, res) => {
     req.models.convention.find({}, { autoFetch: true }, (err, conventions) => {
@@ -73,16 +75,16 @@ router.post('/new', (req, res) => {
 const patterns = ['The Tales of @', 'The Adventure of @', '@ of The Seven Kingdoms', 'The Sad Story of @', '@ The Great'];
 router.post('/gen', (req, res) => {
     randomAnimal('title').then((animal) => {
-        req.models.host.find({}, { limit: 10 }, (err, hosts) => {
+        req.models.host.find({}, {}, (err, hosts) => {
             if (err) throw err;
-            var host = hosts[Math.floor(Math.random() * hosts.length)];
+            var host = hosts[R.integer(0, hosts.length-1)(E)];
             var host_id = host.login_id;
             var start = randomMoment('2018-12-31', moment().format());
             var end = moment(start).add(Math.random() * 4, 'hours');
             var startTime = moment(start).format('YYYY-MM-DD HH:mm:ss');
             var endTime = moment(end).format('YYYY-MM-DD HH:mm:ss');
-            var pattern = patterns[Math.floor(Math.random() * patterns.length)];
-            var room_id = Math.ceil(Math.random() * 4);
+            var pattern = patterns[R.integer(0, patterns.length-1)(E)];
+            var room_id = req.body.room_id || R.integer(1, 4);
             var data = {
                 title: pattern.replace('@', animal),
                 description: randomParagraph({ min: 4, max: 9 }),
